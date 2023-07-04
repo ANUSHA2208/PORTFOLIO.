825912237d0953fc2f417517cec502ca33929744
prkskrs <prakash@divii.com>
2023-07-04 22:23:01 +0530

    contact us trigger added

--- src/components/Button/Footer.jsx
+++ src/components/Button/Footer.jsx
@@ -1,44 +1,71 @@
-import React,{useState} from "react";
-import {FaWhatsappSquare,FaLinkedin} from 'react-icons/fa'
+import React, { useState } from "react";
+import { FaWhatsappSquare, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   const [year, setYear] = useState(new Date().getFullYear());
-  
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
   const handleClick = () => {
     const currentYear = new Date().getFullYear();
     if (year !== currentYear) {
       setYear(currentYear);
     }
   };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const mailtoUrl = `mailto:anushapatil2208@gmail.com?subject=Contact%20Form%20Submission&body=${encodeURIComponent(
+      `Email: ${email}%0D%0AMessage: ${message}`
+    )}`;
+    window.location.href = mailtoUrl;
+    setEmail("");
+    setMessage("");
+  };
+
   return (
     <div className="pt-24">
-      <footer class="footer-distributed">
-        <div class="footer-left">
-
-          <h4 class="footer-links">
-            <a href="mailto:anushapatil2208@gmail.com">Email</a> · <a href="https://www.linkedin.com/in/anusha-patil-167b79162/">Linkedin</a> · <a href="tel:8971232721">Phone no</a>
+      <footer className="footer-distributed">
+        <div className="footer-left">
+          <h4 className="footer-links">
+            <a href="mailto:anushapatil2208@gmail.com">Email</a> ·{" "}
+            <a href="https://www.linkedin.com/in/anusha-patil-167b79162/">
+              Linkedin
+            </a>{" "}
+            · <a href="tel:8971232721">Phone no</a>
           </h4>
 
-          <div class="footer-company-name" onClick={handleClick}>&copy; {year}</div>
-  
-
-          {/* <div class="footer-icons">
-            <a href="#">
-              <FaWhatsappSquare className="w-full h-full"/>
-            </a>
-            <a href="#">
-            <FaLinkedin className="w-full h-full"/>
-            </a>
-          </div> */}
+          <div className="footer-company-name" onClick={handleClick}>
+            &copy; {year}
+          </div>
         </div>
 
-        <div class="footer-right">
+        <div className="footer-right">
           <h2 className="text-2xl pb-2">Contact Us</h2>
 
-          <form action="#" method="post">
-            <input type="text" name="email" placeholder="Email" />
-            <textarea name="message" placeholder="Message"></textarea>
-            <button>Send</button>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={email}
+              onChange={handleEmailChange}
+            />
+            <textarea
+              name="message"
+              placeholder="Message"
+              value={message}
+              onChange={handleMessageChange}
+            ></textarea>
+            <button type="submit">Send</button>
           </form>
         </div>
       </footer>
