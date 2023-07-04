bfa2444e0055e4e03b4eec039041974782c5ee6c
Anusha Patil <anushapatil2208@gmail.com>
2023-07-05 00:29:40 +0530

    gallery1

--- src/components/Button/Work.jsx
+++ src/components/Button/Work.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Gallery from './Gallery'
-import img from '../../assets/images/nft.png'
+import img from '../../assets/images/Page 2 - NFT - Figma 2023-07-05 00-26-21.gif'
 import img1 from '../../assets/images/yono.png'
 import img2 from '../../assets/images/digi.png'
 import quillbot from '../../assets/images/quillbot.png'
-import gif from '../../assets/images/Untitled video - Made with Clipchamp (1).gif'
+
 
 const Work = () => {
   return (
     <div>
-       <Gallery name="NFT Marketplace" link="https://medium.com/@anushapatil2208/nft-marketplace-ux-case-study-c2848f645254" category="Personal" image={gif} />
+       <Gallery name="NFT Marketplace" link="https://medium.com/@anushapatil2208/nft-marketplace-ux-case-study-c2848f645254" category="Personal" image={img} />
        <Gallery name="Redesigning YONO SBI Banking App" link="https://medium.com/@anushapatil2208/case-study-redesigning-a-mobile-banking-app-ae5cf0e8e21a" category="Personal" image={img1} />
        <Gallery name="Digiayurveda" category="Client" image={img2} />
        <Gallery name="Quillbot mobile version" category="Personal" image={quillbot} link='https://medium.com/@anushapatil2208/case-study-quillbot-mobile-app-b99eb85f5b2f'/>              
